acef2ef3f60d34aa499d0f6be6affcbc2e824b42
Hadeed Ul Hassan <hadeed.hassan189@gmail.com>
2026-03-05 03:03:48 +0500

    Implement logger

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import morgan from 'morgan';
+import logger from './config/logger.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
 
 app.use(helmet());
 
 app.get('/', (req, res) => {
+  logger.info('Received a request to the root endpoint');
   res.send('Hello this is Zenora. Backend for learning platform!!');
 });
 
